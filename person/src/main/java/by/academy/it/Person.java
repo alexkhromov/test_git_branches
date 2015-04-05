004e9c03a327d4ac432b5bcc0d6a1973134ffa56
alexkhromov <hrom1981@gmail.com>
2015-04-05 05:09:13 -0500

    update person class ( eaquals / hashcode / toString )

--- person/src/main/java/by/academy/it/Person.java
+++ person/src/main/java/by/academy/it/Person.java
@@ -6,45 +6,58 @@ public class Person {
     private String name;
     private String surName;
 
-    public Person( Integer age, String name, String surName ) {
-
-        this.age = age;
-        this.name = name;
-        this.surName = surName;
-    }
-
     public Integer getAge() {
-
         return age;
     }
 
-    public void setAge( Integer age ) {
-
+    public void setAge(Integer age) {
         this.age = age;
     }
 
     public String getName() {
-
         return name;
     }
 
-    public void setName( String name ) {
-
+    public void setName(String name) {
         this.name = name;
     }
 
     public String getSurName() {
-
         return surName;
     }
 
-    public void setSurName( String surName ) {
-
+    public void setSurName(String surName) {
         this.surName = surName;
     }
 
-    public String toString() {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
 
-        return "Name: " + name + "\nSurname: " + surName + "\nAge: " + age;
+        Person person = (Person) o;
+
+        if (!age.equals(person.age)) return false;
+        if (!name.equals(person.name)) return false;
+        if (!surName.equals(person.surName)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = age.hashCode();
+        result = 31 * result + name.hashCode();
+        result = 31 * result + surName.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", surName='" + surName + '\'' +
+                '}';
     }
 }
