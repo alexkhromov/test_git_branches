3caf20c4bef7e70f99359f56b20031b07970b3b2
alexkhromov <hrom1981@gmail.com>
2015-04-05 05:00:54 -0500

    revert some changes

--- person/src/main/java/by/academy/it/Person.java
+++ person/src/main/java/by/academy/it/Person.java
@@ -2,44 +2,14 @@ package by.academy.it;
 
 public class Person {
 
-    private Integer age;
+    private int age;
     private String name;
     private String surName;
 
-    public Person( Integer age, String name, String surName ) {
+    public Person( int age, String name, String surName ) {
 
         this.age = age;
         this.name = name;
-        this.surName = surName;
-    }
-
-    public Integer getAge() {
-
-        return age;
-    }
-
-    public void setAge( Integer age ) {
-
-        this.age = age;
-    }
-
-    public String getName() {
-
-        return name;
-    }
-
-    public void setName( String name ) {
-
-        this.name = name;
-    }
-
-    public String getSurName() {
-
-        return surName;
-    }
-
-    public void setSurName( String surName ) {
-
         this.surName = surName;
     }
 
