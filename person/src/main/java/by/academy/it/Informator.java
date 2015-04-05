edbd138c0d70f02dcfa27edee2dc586a5c9dfac3
alexkhromov <hrom1981@gmail.com>
2015-04-05 05:27:45 -0500

    update Informator ( new construction of person )

--- person/src/main/java/by/academy/it/Informator.java
+++ person/src/main/java/by/academy/it/Informator.java
@@ -4,7 +4,10 @@ public class Infomator
 {
     public static void main( String[] args )
     {
-        Person person = new Person( 30, "John", "Smith" );
+        Person person = new Person();
+        person.setAge( 30 );
+        person.setName( "John" );
+        person.setSurName( "Smith" );
         System.out.println( person.toString() );
     }
 }
