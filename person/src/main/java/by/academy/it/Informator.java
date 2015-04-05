9399bea2945f0742ceec7cb7f8643b36606422f8
alexkhromov <hrom1981@gmail.com>
2015-04-05 04:29:47 -0500

    add Informator class to print info about person

---
+++ person/src/main/java/by/academy/it/Informator.java
@@ -0,0 +1,11 @@
+package by.academy.it;
+
+
+public class Infomator 
+{
+    public static void main( String[] args )
+    {
+        Person person = new Person( 30, "John", "Smith" );
+        System.out.println( person.toString() );
+    }
+}
